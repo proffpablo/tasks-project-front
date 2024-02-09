83593034a6e0936a034af26984daaf41fffacf91
proffpablo <pabloandresmartinezrejas@gmail.com>
2024-02-08 23:35:24 -0300

    403 solved 2

--- src/context/TasksContext.jsx
+++ src/context/TasksContext.jsx
@@ -26,7 +26,7 @@ export function TaskProvider({ children }) {
   const getTasks = async () => {
     try {
       const res = await getTasksRequest();
-      localStorage.setItem('token', res.headers.authorization);
+      localStorage.getItem('token');
       setTasks(res.data);
     } catch (error) {
       console.error(error);
