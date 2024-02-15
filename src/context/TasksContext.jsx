bc1aa025a4e88ee74e5e0b4d54adb8969b95133f
proffpablo <pabloandresmartinezrejas@gmail.com>
2024-02-15 01:30:01 -0300

    createTask no return

--- src/context/TasksContext.jsx
+++ src/context/TasksContext.jsx
@@ -29,7 +29,7 @@ export function TaskProvider({ children }) {
 
   const createTask = async (task) => {
     const token = localStorage.getItem('token');
-    return await axios.post('/tasks', task, { headers: { 'Authorization': token } });
+    res = await axios.post('/tasks', task, { headers: { 'Authorization': token } });
   }
 
   const deleteTask = async (id) => {
